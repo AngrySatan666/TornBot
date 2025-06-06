80e078dacc42a70d50dc3e2456a488ec6cff18c3
怒り の 悪魔 <142706418+AngrySatan666@users.noreply.github.com>
2025-06-06 04:17:17 -0400

    TESTING GITTR

--- scripts/Gittr.bat
+++ scripts/Gittr.bat
@@ -2,47 +2,49 @@
 title Gittr
 COLOR 07
 
+:: Admin Check ::
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [91m[ERROR] Not running as administrator. Relaunching with admin rights...
+    timeout /t 3 >nul
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Ensure .git ::
 cd /d "%~dp0.."
-echo Current directory: %cd%
 if exist ".git" (
-    echo .git directory found.
+    echo [92m[INFO] Repo found! && echo 
+    echo [93m[WARNING] Forcing Ownership... && echo 
+    call :OWN
 ) else (
-    echo .git directory NOT found!
+    echo  [91m[ERROR] .git directory NOT found! && echo
 )
 
-for /f "tokens=2 delims=:" %%O in ('icacls . ^| findstr /i "OWNER:"') do set "OWNER=%%O"
-set "OWNER=%OWNER:~1%"  REM Trim leading space
+:OWN
+echo  [0mCurrent user:  [96m%USERNAME% && echo 
+takeown /f . /r /d y >nul
+icacls . /grant:r "%USERNAME%:F" /t >nul
+icacls . /inheritance:e /t >nul
+echo [92m[INFO] Ownership changed to  [96m%USERNAME%. && echo [0m
 
-echo Repo owner:%OWNER%
-echo Current user:%USERNAME%
-
-echo %OWNER% | findstr /I "%USERNAME%" >nul
+git remote -v && echo 
 if errorlevel 1 (
-    echo [93mTaking ownership of the repo as you are not the owner...[0m
-    takeown /f . /r /d y >nul
-    icacls . /setaudit "%USERNAME%:F" /t >nul
-    icacls . /grant:r "%USERNAME%:F" /t >nul
-    icacls . /inheritance:e /t >nul
-    echo [92mOwnership changed to %USERNAME%.[0m
-)
-
-git remote -v
-if errorlevel 1 (
-    echo [91mNo git remote found. Exiting.[97m
+    echo [91m[ERROR] No git remote found. Exiting in 5s && echo 
     timeout /t 5 >nul
     exit /b 1
 )
 
 if "%~1"=="" (
-    set /p "Arg=[92mPlease provide a commit message:  [97m"
+    set /p "Arg=[92mPlease provide a commit message: [95m" && echo 
 ) else (
     set "Arg=%1"
 )
 
 cd /d "%~dp0.."
-git remote -v >nul 2>&1
+git remote -v >nul 2>&1 && echo 
 if errorlevel 1 (
-    echo [91mNo git remote found. Exiting.[97m
+    echo [91m[ERROR] No git remote found. Exiting.[96m && echo 
     timeout /t 5 >nul
     exit /b 1
 )
@@ -51,10 +53,10 @@ git add .
 git commit -m "%Arg%"
 git push
 if errorlevel 1 (
-    echo '[91mGit push failed. Please check your connection or repository settings.[0m'
+    echo [91m[ERROR] Git push failed. Please check your connection or repository settings.[0m && echo 
     exit /b 1
 ) else (
-    echo 'Gittr Pushed!'
+    echo [42m[.35]Gittr Pushed!
 )
 
 Pause
